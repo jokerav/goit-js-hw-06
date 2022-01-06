25f75ef76fbc2c2824cb518760ccfd5590f2d7e0
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-06 23:39:13 +0200

    2.0

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,10 +8,11 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector("#ingredients");
-
+let list = [];
 ingredients.forEach((ingredient) => {
   const item = document.createElement("li");
   item.textContent = ingredient;
   item.classList.add("item");
-  ingredientsList.append(item);
+  list.push(item);
 });
+ingredientsList.append(...list);
