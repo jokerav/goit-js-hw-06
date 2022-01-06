16fe8c558c6dd65a6585c1e80bb7d41c7e3047c3
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-06 20:11:09 +0200

    1.0

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,11 @@
+// выбираем все елементы с тегом .item
 const listCategories = document.querySelectorAll(".item");
-console.log(listCategories);
 console.log(`Number of categories: ${listCategories.length}`);
+
+// выбираем заголовки списков и помещаем их в массив
+const listHeaders = document.querySelectorAll(".item h2");
+listHeaders.forEach((element) => {
+  console.log(`Category: ${element.textContent}`);
+  // для каждого заголовка ищем соседа (список) и выводим количество его елемеентов
+  console.log(`Elements: ${element.nextElementSibling.children.length}`);
+});
