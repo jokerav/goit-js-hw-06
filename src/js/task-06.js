2a49bca238fdcdc5a262827d5e67f9443ab91d82
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-07 22:23:29 +0200

    6

---
+++ src/js/task-06.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector("#validation-input");
+inputEl.addEventListener("blur", chekInputLength);
+function chekInputLength() {
+  const inputText = inputEl.value;
+
+  if (inputText.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  } else inputEl.classList.add("invalid");
+}
