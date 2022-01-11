ce7e71f1338dffa97b5b1df31d8724080ba893f7
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-11 07:42:22 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputCountBoxesEl = document.querySelector("input");
+const creatBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+creatBtn.addEventListener(
+  "click",
+  createBoxes(inputCountBoxesEl.valueAsNumber)
+);
+
+function createBoxes(amount) {
+  let boxesArr = [];
+  let size = 30;
+  for (const i = 0; i < amount; i += 1) {
+    let box = document.createElement("div");
+    box.style.height = `${size}px`;
+    box.style.width = `${size}px`;
+    box.style.color = getRandomHexColor();
+    boxesArr.push(box);
+  }
+  console.log(amount);
+  console.log(boxesArr);
+  boxes.append(...boxesArr);
+  //return boxesArr;
+}
