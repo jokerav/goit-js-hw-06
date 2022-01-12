cf6d9b19630bfb30d71076b79134ba52ffe86e44
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-12 19:19:35 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,10 +10,17 @@ const boxes = document.querySelector("#boxes");
 creatBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
 
-function createBoxes(amount) {
-  console.log(amount);
-  amount = inputCountBoxesEl.valueAsNumber;
-  console.log(amount);
+// В условии написано "Создай функцию createBoxes(amount), которая принимает один параметр - число."
+// У меня получился вариант без параметра(строки 23 - 24).
+// После долгих плясок с бубном получился таки вариант функции с параметром (строки 20-21), но непонятно как он работает.
+// Если возможно, напишите, пожалуйта что происходит в закоментированых строках. Заранее благодарен
+
+// function createBoxes(amount) {
+//   amount = inputCountBoxesEl.valueAsNumber;
+
+function createBoxes() {
+  const amount = inputCountBoxesEl.valueAsNumber;
+  destroyBoxes();
   let boxesArr = [];
   let size = 30;
   for (let i = 0; i < amount; i += 1) {
